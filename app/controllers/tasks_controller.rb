0e9d603f90b2713b2dba4b986d15ae90dbfa498b
Pedro Cobos <pedro.cobos@live.com>
2020-11-10 12:06:43 -0600

    added route for editing tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,16 +10,27 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
-
+  
   def create
+    # raise
     @task = Task.new(params[:task_params])
-    @task.save
+    @task.save!
+    redirect_to root_path
   end
   
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task_params])
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
   
 end
