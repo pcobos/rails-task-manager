702a27cc9313b14d028445df55a43995ef75d472
Pedro Cobos <pedro.cobos@live.com>
2020-11-09 23:00:36 -0600

    added task model and tasks controller

---
+++ db/migrate/20201110044905_add_column_to_tasks.rb
@@ -0,0 +1,6 @@
+class AddColumnToTasks < ActiveRecord::Migration[6.0]
+  def change
+    add_column :tasks, :completed, :boolean, default: false
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
